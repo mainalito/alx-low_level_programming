5d2a64116bf6d1e8004401bb5242daf00073969d
mainalito <maich231@gmail.com>
2022-09-09 07:46:18 -0700

    0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	if (n > 0)
+	{
+		printf("is positie");
+	}
+	else
+	{
+		printf("is negative");
+	}
+}
