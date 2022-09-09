3464c86ce83036647ea9fd68c30601b9470d42a7
mainalito <maich231@gmail.com>
2022-09-09 09:30:16 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - print all except q and e
+*
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
