225a43de65786b8d87fb114b33910601121d3459
mainalito <maich231@gmail.com>
2022-09-30 13:23:07 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * main - multiplies two numbers
  * @argc: argument count
@@ -10,14 +11,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, total = 1;
+	int prod;
 
 	if (argc != 3)
+	{
 		printf("Error\n");
 		return (1);
+	}
 
-	for (i = 1; i < argc; i++)
-		total = total * atoi(argv[i]);
-	printf("%d\n", total);
+	prod = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", prod);
+
 	return (0);
 }
