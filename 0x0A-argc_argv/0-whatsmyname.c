450848b085efeef99090371b0a5be3a419ae76c9
mainalito <maich231@gmail.com>
2022-09-30 12:54:42 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - prints the name of this program
+ * @argc: argv array size
+ * @argv: argument array
+ *
+ * Return: Always 0 (success)
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
